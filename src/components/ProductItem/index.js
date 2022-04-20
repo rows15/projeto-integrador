2de455f16ab8327acd27c3577d4e207cc387e509
Fran <francieliceleghim2013@gmail.com>
2022-04-20 19:03:50 -0300

    carrinho feito

--- src/components/ProductItem/index.js
+++ src/components/ProductItem/index.js
@@ -11,8 +11,6 @@ export default function ProductItem({ prmProduct }) {
         clearCart
     } = useContext(CartContext);
 
-    console.log('productsCart', productsCart);
-
     return (
 
         <ul style={{margin:"30px", padding:"10px",alignItems:"center" }}>
@@ -21,19 +19,11 @@ export default function ProductItem({ prmProduct }) {
                     <img  style={{ width: 200, height:150,margin: "50px 120px"}} src={prmProduct.image} />
                     <h3 style={{color: "black", margin: "10px",textAlign: "center"}}>{prmProduct.title}</h3>
                     <p style={{color: "black", margin: "10px ",textAlign: "center"}}>{prmProduct.description}</p>
-                    <p style={{ color: "black", margin: "10px", textAlign: "center" }}>{prmProduct.price}</p>
+                    <p style={{ color: "black", margin: "10px", textAlign: "center" }}>R$ {prmProduct.price}</p>
 
                 </Link>
-                <button onClick={() => addProducToCart(prmProduct)} style={{ display: "inline-block", background: "#003f69", border: "none", color: "white", padding: "10px 32px", textAlign: "center", fontSize: "16px", margin: "0px 155px", borderRadius: "10px",boxShadow: "5px 5px 5px grey"}}>
+                <button onClick={() => addProducToCart(prmProduct)} style={{ display: "inline-block", background: "#003f69", border: "none", color: "white", padding: "10px 32px", textAlign: "center", fontSize: "16px", margin: "0px 155px", borderRadius: "10px", boxShadow: "5px 5px 5px grey", width: "150px"}}>
                     Adicionar
-                </button>
-                <input value={ (productsCart.find(p => p.id === prmProduct.id)?.qtd | 0) * prmProduct.price } />
-                <button  onClick={() => removeProductToCart(prmProduct.id)} style={
-                    (productsCart.map(s=>s.id).includes(prmProduct.id)) ?
-                        { display: "inline-block", background: "#003f69", border: "none", color: "white", padding: "10px 32px", textAlign: "center", fontSize: "16px", margin: "0px 155px", borderRadius: "10px",boxShadow: "5px 5px 5px grey"}
-                    : { display: "none" }
-                }>
-                    Remover
                 </button>
             </li>
         </ul>
