ca031d65fd37525f1c190862096f8a803968eab8
rows <marceloxpaduan@gmail.com>
2022-04-20 23:11:20 -0300

    volteipraApiCurta

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://ec2-18-118-21-85.us-east-2.compute.amazonaws.com:8080/",
+  baseURL: "http://18.118.21.85:8080/",
 });
 
 export default api;
