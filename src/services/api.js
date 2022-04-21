7c087bdab1301ae1fc471967b668bb0cff96bdc6
rows <marceloxpaduan@gmail.com>
2022-04-20 23:00:32 -0300

    mudei pra http e link longo

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://18.118.21.85:8080/",
+  baseURL: "http://ec2-18-118-21-85.us-east-2.compute.amazonaws.com:8080/",
 });
 
 export default api;
